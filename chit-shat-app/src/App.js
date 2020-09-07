0625271251efe1d49325facd3cab4a10b46ac96d
knezza <kneza@outlook.com>
2020-09-07 17:29:20 +0200

    Integration

--- chit-shat-app/src/App.js
+++ chit-shat-app/src/App.js
@@ -1,16 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState} from 'react';
 import './App.css';
 import Sidebar from './Sidebar';
 import Chat from './Chat';
+import Pusher from 'pusher-js';
+import axios from './axios';
 
 function App() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    axios.get('/messages/sync').then(res => {
+      setMessages(res.data);
+    })
+  }, [messages]); // Fix za poruke ali sjebe pushera
+
+  useEffect(() => {
+
+    const pusher = new Pusher('b5b671d57e844c3ca149', {
+      cluster: 'eu'
+    });
+
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted', (newMessage) => {
+      // alert(JSON.stringify(newMessage)); Debugging tool
+      setMessages([...messages,newMessage]);
+    });
+
+    return () => {
+      channel.unbind_all(); //Hmmm....So it doesnt mess up browser if group starts spamming
+      channel.unsubscribe();
+    };
+
+  }, [messages]);
+
+  console.log(messages);
+
   return (
     <div className="app">
       <div className="app__body">
-        <Sidebar/>
-        <Chat />
+        <Sidebar />
+        <Chat messages={messages}/>
       </div>
-      
     </div>
   );
 }
--- chit-shat-app/src/Chat.js
+++ chit-shat-app/src/Chat.js
@@ -7,7 +7,7 @@ import MoreVertOutlinedIcon from '@material-ui/icons/MoreVertOutlined'
 import InsertEmoticonOutlinedIcon from '@material-ui/icons/InsertEmoticonOutlined'
 import MicIcon from '@material-ui/icons/Mic'
 
-function Chat() {
+function Chat({messages}) {
     return (
         <div className="chat">
             <div className="chat__header">
@@ -32,54 +32,14 @@ function Chat() {
             </div>
 
             <div className="chat__body">
-                <p className="chat__message">
-                    <span className="chat__name">Ivan Knezovic</span>
-                    Hello World!
 
-                    <span className="chat__timestamp">{new Date().toUTCString()}</span>
+                {messages.map((message) => (
+                    <p className={`chat__message ${message.received && "chat__reciever"}`}>
+                    <span className="chat__name">{message.name}</span>
+                    {message.message}
+                    <span className="chat__timestamp">{message.timestamp}</span>
                 </p>
-
-                <p className="chat__message chat__reciever">
-                    <span className="chat__name">World</span>
-                    Eat shit cunt!
-
-                    <span className="chat__timestamp">{new Date().toUTCString()}</span>
-                </p>
-
-                <p className="chat__message">
-                    <span className="chat__name">Ivan Knezovic</span>
-                    Hello World!
-
-                    <span className="chat__timestamp">{new Date().toUTCString()}</span>
-                </p>
-
-                <p className="chat__message chat__bot3000">
-                    <span className="chat__name">ChatBot3000</span>
-                    Eat shit cunt!
-
-                    <span className="chat__timestamp">{new Date().toUTCString()}</span>
-                </p>
-
-                <p className="chat__message chat__reciever">
-                    <span className="chat__name">World</span>
-                    Eat shit cunt!
-
-                    <span className="chat__timestamp">{new Date().toUTCString()}</span>
-                </p>
-
-                <p className="chat__message">
-                    <span className="chat__name">Ivan Knezovic</span>
-                    Hello World!
-
-                    <span className="chat__timestamp">{new Date().toUTCString()}</span>
-                </p>
-
-                <p className="chat__message chat__reciever">
-                    <span className="chat__name">World</span>
-                    Eat shit cunt!
-
-                    <span className="chat__timestamp">{new Date().toUTCString()}</span>
-                </p>
+                ))}
 
             </div>
 
